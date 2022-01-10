fb256b54938b4fef42076e4490c97342896414c9
Christian Dobbie <chris@christiandobbie.com>
2022-01-10 18:52:55 +0000

    Revert "Change updateQueue to a Set"
    This reverts commit 7e52927084fe97c8e34a03cf4fb49840cb1f2219.

--- svc/items.js
+++ svc/items.js
@@ -16,7 +16,7 @@ const port = config.PORT || config.ITEMS_PORT;
 let discoveredItems;
 let bazaarProducts = [];
 let itemList = {};
-const updateQueue = new Set();
+const updateQueue = [];
 
 (async function init() {
   try {
@@ -72,7 +72,7 @@ app.post('/', (request, response, _callback) => {
   let updates = false;
   items.forEach((item) => {
     const { id } = item;
-    if (!discoveredItems.has(id) || updateQueue.has(id)) {
+    if (!discoveredItems.has(id) || updateQueue.includes(id)) {
       updates = true;
       logger.info(`Found new item ID ${id}`);
       if (item.texture === null) {
@@ -84,9 +84,6 @@ app.post('/', (request, response, _callback) => {
       delete item.id;
       itemList[id] = item;
       discoveredItems.add(id);
-      if (updateQueue.has(id)) {
-        updateQueue.delete(id);
-      }
     }
   });
   if (updates) {
@@ -98,7 +95,7 @@ app.delete('/:id', (request, response, _callback) => {
   const { id } = request.params;
   if (id in itemList) {
     logger.info(`Adding item ${id} to update queue`);
-    updateQueue.add(id);
+    updateQueue.push(id);
   }
   response.json({ status: 'ok' });
 });
