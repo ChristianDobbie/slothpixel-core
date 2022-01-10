1dbb927a632d7ce1448be287febd0319aa417ea4
Christian Dobbie <chris@christiandobbie.com>
2022-01-10 18:52:55 +0000

    Revert "Add profile name to /skyblock/profile/ endpoint"
    This reverts commit fe35a7c83123f2dd78d5477d6c89ff92317ef9fe.

--- store/buildSkyBlockProfiles.js
+++ store/buildSkyBlockProfiles.js
@@ -92,13 +92,11 @@ async function buildProfile(uuid, id = null) {
   }
 
   // eslint-disable-next-line arrow-body-style
-  returned_Profile = await cachedFunction(`skyblock_profile:${profile_id}`, async () => {
+  return cachedFunction(`skyblock_profile:${profile_id}`, async () => {
     // insertSkyBlockProfile(profile);
 
     return getProfileData(profile_id);
   }, { cacheDuration: 600 });
-  returned_Profile["cute_name"] = profiles[profile_id].cute_name.toLowerCase() //add profile name to the end of the profile
-  return returned_Profile //return appended profile object
 }
 
 module.exports = {
